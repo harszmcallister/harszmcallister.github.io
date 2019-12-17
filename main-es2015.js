45e0adf4083216ca471668f92271b923d9e706ea
harszmcallister <55360759+harszmcallister@users.noreply.github.com>
2019-12-17 21:54:03 +0530

    changes

---
+++ main-es2015.js
@@ -0,0 +1,216 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["main"],{
+
+/***/ "./$$_lazy_route_resource lazy recursive":
+/*!******************************************************!*\
+  !*** ./$$_lazy_route_resource lazy namespace object ***!
+  \******************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncaught exception popping up in devtools
+	return Promise.resolve().then(function() {
+		var e = new Error("Cannot find module '" + req + "'");
+		e.code = 'MODULE_NOT_FOUND';
+		throw e;
+	});
+}
+webpackEmptyAsyncContext.keys = function() { return []; };
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+module.exports = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = "./$$_lazy_route_resource lazy recursive";
+
+/***/ }),
+
+/***/ "./node_modules/raw-loader/index.js!./src/app/app.component.html":
+/*!**************************************************************!*\
+  !*** ./node_modules/raw-loader!./src/app/app.component.html ***!
+  \**************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<!DOCTYPE html>\n<html>\n<head>\n  <style>\n    #map{\n      height: 500px;\n      width : 100%\n  }\n  </style>\n</head>\n<body>\n<!-- contact -->\n<section id=\"contact\" class=\"section-padding\">\n  <div class=\"container\">\n    <div class=\"row\">\n      <div class=\"col-md-12 text-center\">\n        <h1 class=\"header-h\">Book Your table</h1>\n      </div>\n    </div>\n    <div class=\"row msg-row\">\n      <div class=\"col-md-4 col-sm-4 mr-15\">\n        <div class=\"media-2\">\n          <div class=\"media-left\">\n            <div class=\"contact-phone bg-1 text-center\"><span class=\"phone-in-talk fa fa-phone\"></span></div>\n          </div>\n          <div class=\"media-body\">\n            <h4 class=\"dark-blue regular\">Phone Numbers</h4>\n            <p class=\"light-blue regular alt-p\">+440 875369208 - <span class=\"contacts-sp\">Phone Booking</span></p>\n          </div>\n        </div>\n        <div class=\"media-2\">\n          <div class=\"media-left\">\n            <div class=\"contact-email bg-14 text-center\"><span class=\"hour-icon fa fa-clock-o\"></span></div>\n          </div>\n          <div class=\"media-body\">\n            <h4 class=\"dark-blue regular\">Opening Hours</h4>\n            <p class=\"light-blue regular alt-p\"> Monday to Friday 09.00 - 24:00</p>\n            <p class=\"light-blue regular alt-p\">\n              Friday and Sunday 08:00 - 03.00\n            </p>\n          </div>\n        </div>\n      </div>\n      <div class=\"col-md-8 col-sm-8\">\n        <form  class=\"contactForm\">\n          <div id=\"sendmessage\">Your booking request has been sent. Thank you!</div>\n          <div id=\"errormessage\"></div>\n          <div class=\"col-md-6 col-sm-6 contact-form pad-form\">\n            <div class=\"form-group label-floating is-empty\">\n              <input type=\"text\" name=\"name\" class=\"form-control\" id=\"name\" placeholder=\"Your Name\" data-rule=\"minlen:4\" data-msg=\"Please enter at least 4 chars\" />\n              <div class=\"validation\"></div>\n            </div>\n\n          </div>\n          <div class=\"col-md-6 col-sm-6 contact-form\">\n            <div class=\"form-group\">\n              <input type=\"date\" class=\"form-control label-floating is-empty\" name=\"date\" id=\"date\" placeholder=\"Date\" data-rule=\"required\" data-msg=\"This field is required\" />\n              <div class=\"validation\"></div>\n            </div>\n          </div>\n          <div class=\"col-md-6 col-sm-6 contact-form pad-form\">\n            <div class=\"form-group\">\n              <input type=\"email\" class=\"form-control label-floating is-empty\" name=\"email\" id=\"email\" placeholder=\"Your Email\" data-rule=\"email\" data-msg=\"Please enter a valid email\" />\n              <div class=\"validation\"></div>\n            </div>\n          </div>\n          <div class=\"col-md-6 col-sm-6 contact-form\">\n            <div class=\"form-group\">\n              <input type=\"time\" class=\"form-control label-floating is-empty\" name=\"time\" id=\"time\" placeholder=\"Time\" data-rule=\"required\" data-msg=\"This field is required\" />\n              <div class=\"validation\"></div>\n            </div>\n          </div>\n          <div class=\"col-md-6 col-sm-6 contact-form\">\n            <div class=\"form-group\">\n              <input type=\"text\" class=\"form-control label-floating is-empty\" name=\"phone\" id=\"phone\" placeholder=\"Phone\" data-rule=\"required\" data-msg=\"This field is required\" />\n              <div class=\"validation\"></div>\n            </div>\n          </div>\n          <div class=\"col-md-6 col-sm-6 contact-form\">\n            <div class=\"form-group\">\n              <input type=\"text\" class=\"form-control label-floating is-empty\" name=\"people\" id=\"people\" placeholder=\"People\" data-rule=\"required\" data-msg=\"This field is required\" />\n              <div class=\"validation\"></div>\n            </div>\n          </div>\n          <div class=\"col-md-12 contact-form\">\n            <div class=\"form-group label-floating is-empty\">\n              <textarea class=\"form-control\" name=\"message\" rows=\"5\" rows=\"3\" data-rule=\"required\" data-msg=\"Please write something for us\" placeholder=\"Message\"></textarea>\n              <div class=\"validation\"></div>\n            </div>\n\n          </div>\n          <div class=\"col-md-12 btnpad\">\n            <div class=\"contacts-btn-pad\">\n              <button class=\"contacts-btn\">Book Table</button>\n            </div>\n          </div>\n        </form>\n      </div>\n    </div>\n  </div>\n</section>\n<!-- / contact -->\n  <!-- Start Map section -->\n  <section>\n    <iframe src=\"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9207.358598888495!2d-85.64847801496286!3d30.183918972289003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x2320479d70eb6202!2sDillard&#39;s!5e0!3m2!1sbn!2sbd!4v1462359735720\" width=\"100%\" height=\"100%\" frameborder=\"0\"allowfullscreen></iframe>\n  </section>\n  <!-- End Map section -->\n<!-- footer -->\n<footer class=\"footer text-center\">\n  <div class=\"footer-top\">\n    <div class=\"row\">\n      <div class=\"col-md-offset-3 col-md-6 text-center\">\n        <div class=\"widget\">\n          <h4 class=\"widget-title\">Kerala Restaurant</h4>\n          <address>Via la Spezia, 10/B<br>00182 Rome RM, Italy</address>\n          <div class=\"social-list\">\n            <a href=\"#\"><i class=\"fa fa-twitter\" aria-hidden=\"true\"></i></a>\n            <a href=\"#\"><i class=\"fa fa-facebook\" aria-hidden=\"true\"></i></a>\n          </div>\n        </div>\n      </div>\n    </div>\n  </div>\n</footer>\n<!-- / footer -->\n<script>\n  function initMap(){\n    var map = new google.maps.Map(document.getElementById(\"map\"),{\n      zoom: 4, center: {lat:42.3601,lng:-71.0589}\n    });\n  }\n</script>\n<script async defer src=\"https://maps.googleapis.com/maps/api/js?key=AIzaSyC2CuaUonE6CH6KM9Fgni0w9fNmHcl_kgY&callback=initMap\">\n\n</script>\n<router-outlet></router-outlet>\n</body>\n</html>\n\n\n\n\n"
+
+/***/ }),
+
+/***/ "./src/app/app-routing.module.ts":
+/*!***************************************!*\
+  !*** ./src/app/app-routing.module.ts ***!
+  \***************************************/
+/*! exports provided: AppRoutingModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppRoutingModule", function() { return AppRoutingModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm2015/core.js");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm2015/router.js");
+
+
+
+const routes = [];
+let AppRoutingModule = class AppRoutingModule {
+};
+AppRoutingModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+        imports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"].forRoot(routes)],
+        exports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"]]
+    })
+], AppRoutingModule);
+
+
+
+/***/ }),
+
+/***/ "./src/app/app.component.css":
+/*!***********************************!*\
+  !*** ./src/app/app.component.css ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL2FwcC5jb21wb25lbnQuY3NzIn0= */"
+
+/***/ }),
+
+/***/ "./src/app/app.component.ts":
+/*!**********************************!*\
+  !*** ./src/app/app.component.ts ***!
+  \**********************************/
+/*! exports provided: AppComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppComponent", function() { return AppComponent; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm2015/core.js");
+
+
+let AppComponent = class AppComponent {
+    constructor() {
+        this.title = 'delicious';
+    }
+};
+AppComponent = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+        selector: 'app-root',
+        template: __webpack_require__(/*! raw-loader!./app.component.html */ "./node_modules/raw-loader/index.js!./src/app/app.component.html"),
+        styles: [__webpack_require__(/*! ./app.component.css */ "./src/app/app.component.css")]
+    })
+], AppComponent);
+
+
+
+/***/ }),
+
+/***/ "./src/app/app.module.ts":
+/*!*******************************!*\
+  !*** ./src/app/app.module.ts ***!
+  \*******************************/
+/*! exports provided: AppModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppModule", function() { return AppModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_platform_browser__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/platform-browser */ "./node_modules/@angular/platform-browser/fesm2015/platform-browser.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm2015/core.js");
+/* harmony import */ var _app_routing_module__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./app-routing.module */ "./src/app/app-routing.module.ts");
+/* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
+
+
+
+
+
+let AppModule = class AppModule {
+};
+AppModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_2__["NgModule"])({
+        declarations: [
+            _app_component__WEBPACK_IMPORTED_MODULE_4__["AppComponent"]
+        ],
+        imports: [
+            _angular_platform_browser__WEBPACK_IMPORTED_MODULE_1__["BrowserModule"],
+            _app_routing_module__WEBPACK_IMPORTED_MODULE_3__["AppRoutingModule"]
+        ],
+        providers: [],
+        bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_4__["AppComponent"]]
+    })
+], AppModule);
+
+
+
+/***/ }),
+
+/***/ "./src/environments/environment.ts":
+/*!*****************************************!*\
+  !*** ./src/environments/environment.ts ***!
+  \*****************************************/
+/*! exports provided: environment */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "environment", function() { return environment; });
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+const environment = {
+    production: false
+};
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+
+/***/ }),
+
+/***/ "./src/main.ts":
+/*!*********************!*\
+  !*** ./src/main.ts ***!
+  \*********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm2015/core.js");
+/* harmony import */ var _angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/platform-browser-dynamic */ "./node_modules/@angular/platform-browser-dynamic/fesm2015/platform-browser-dynamic.js");
+/* harmony import */ var _app_app_module__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app/app.module */ "./src/app/app.module.ts");
+/* harmony import */ var _environments_environment__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./environments/environment */ "./src/environments/environment.ts");
+
+
+
+
+if (_environments_environment__WEBPACK_IMPORTED_MODULE_3__["environment"].production) {
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["enableProdMode"])();
+}
+Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformBrowserDynamic"])().bootstrapModule(_app_app_module__WEBPACK_IMPORTED_MODULE_2__["AppModule"])
+    .catch(err => console.error(err));
+
+
+/***/ }),
+
+/***/ 0:
+/*!***************************!*\
+  !*** multi ./src/main.ts ***!
+  \***************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! C:\Users\harsz\OneDrive\Desktop\Deli\delicious\src\main.ts */"./src/main.ts");
+
+
+/***/ })
+
+},[[0,"runtime","vendor"]]]);
+//# sourceMappingURL=main-es2015.js.map
